f5545f0d02f66faf01b9a60a8b1712b374e41385
Dustin Johnson <dustin@oedo.co>
2026-04-11 00:08:33 -0500

    adding stylus and pug / jade

--- nvim_omarchy/.config/nvim/lua/plugins/coffeescript.lua
+++ nvim_omarchy/.config/nvim/lua/plugins/coffeescript.lua
@@ -3,4 +3,12 @@ return {
     "kchmck/vim-coffee-script",
     ft = { "coffee", "litcoffee" },
   },
+  {
+    "digitaltoad/vim-pug",
+    ft = { "pug", "jade" },
+  },
+  {
+    "iloginow/vim-stylus",
+    ft = { "stylus" },
+  },
 }
